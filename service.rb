b85d07fb8d90073f101b181c32b163dfb824de14
macarci <macarci@gmail.com>
2016-03-14 13:21:53 -0400

    Enclosing config in Sinatra class app

--- service.rb
+++ service.rb
@@ -5,19 +5,26 @@ require './models'
 class Service < ::Sinatra::Base
 
   configure do
+
     Mongoid::Config.load!('mongoid.yml')
+
+    enable :logging
   end
 
   before do
+    logger.info 'Before'
     if (key = params[:key]) && (user = User.where(unique_key: key).first)
+      logger.info 'Key Ok!'
       Thread.current[:current_account] = user.account
     else
+      logger.info 'Invalid Key'
       Setup::Notification.create(message: "Accessing service with an invalid user key: #{key}")
       halt 401
     end
   end
 
   get '/schema' do
+  logger.info 'Resolving Schema'
     if (schema = Setup::Schema.where(namespace: params[:ns], uri: params[:uri]).first)
       schema.cenit_ref_schema(service_url: request.base_url)
     else
